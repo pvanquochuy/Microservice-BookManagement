f9f06a7e3dfef5bfa28d2cc0ba9e98a294264933
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-08-18 23:31:02 +0700

    inital

--- src/main/java/com/example/job_portal/usermanagement/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.job_portal.usermanagement.enums;
-
-public enum Role {
-    ADMIN,
-    USER
-}
--- src/main/java/com/example/job_portal/usermanagement/repository/InvalidatedTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.job_portal.usermanagement.repository;
-
-import com.example.job_portal.usermanagement.entity.InvalidatedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
-}
